0f40957d35fc70be22f14aee2e97cb39935dd4b7
mtvspec <mtv.spec@gmail.com>
2015-09-06 14:41:44 +0600

    creates angular-express-seed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // GET home page
-router.get('/', function(request, response) {
-  response.render('index', { title: 'Express' });
+router.get('/', function(request, response){
+  response.end();
 });
 
 module.exports = router;
