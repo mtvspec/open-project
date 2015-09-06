ebd5332fffde8223e4d99b921d0f5d283ff98ff3
mtvspec <mtv.spec@gmail.com>
2015-09-06 14:00:06 +0600

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(error, request, response, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(error);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(error, request, response) {
+    response.status(error.status || 500);
+    response.render('error', {
+      message: error.message,
+      error: error
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(error, request, response) {
+  response.status(error.status || 500);
+  response.render('error', {
+    message: error.message,
+    error: {}
+  });
+});
+
+module.exports = app;
